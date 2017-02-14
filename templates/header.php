147957a02ff9e95f328a53161ec8c404be1ff1f5
Vysotskiy Alexander <vysotskij.av@gmail.com>
2017-02-14 08:06:56 +0300

    remove all responsive from styles

--- templates/header.php
+++ templates/header.php
@@ -4,6 +4,7 @@
     <head>
         <meta charset="utf-8" />
         <title>TheQA - Российская компания по тестированию ПО</title>
+        <!--<meta name="viewport" content="width=device-width, minimum-scale=1.0">-->
         <meta name="keywords" content="" />
         <meta name="description" content="" />
         <link rel="stylesheet" href="css/bootstrap.min.css"  />
