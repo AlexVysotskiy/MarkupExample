38133e47e8b118e3d4205078d75f2f241c866219
Vysotskiy Alexander <vysotskij.av@gmail.com>
2017-02-15 12:33:42 +0300

    fixes for test view

---
+++ competence.php
@@ -0,0 +1,70 @@
+<?php
+
+$map = array();
+
+$uri = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '/';
+
+$routeMap = array(
+    '/' => 'main',
+    '/services' => 'services',
+    '/company' => 'company',
+    '/qa' => 'qa',
+    '/projects' => 'projects',
+    '/competence' => 'competence',
+    '/closed' => 'closed',
+);
+
+$serviceUrl = array(
+    '/sendmail'
+);
+
+$baseLink = '';
+
+if (in_array($uri, $serviceUrl)) {
+
+    $response = array('success' => 1);
+
+    if ($uri == '/sendmail') {
+
+        $name = strip_tags(trim(@$_REQUEST['name']));
+        $phone = strip_tags(trim(@$_REQUEST['phone']));
+        $desc = strip_tags(trim(@$_REQUEST['desc']));
+
+        if ($name && $phone && $desc) {
+
+            $from = 'request@theQA.ru';
+            $message = '<div><p>Заявка на консультацию с сайта <b>' . $_SERVER['SERVER_NAME'] . '</b></p>'
+                    . '<p><b>Контактные данные:</b><br />'
+                    . '&emsp;Имя: ' . $name . '<br />'
+                    . '&emsp;Телефон: ' . $phone
+                    . '&emsp;Описание проекта: ' . $desc . '</p>'
+                    . '</div>';
+            $message = wordwrap($message, 70);
+
+            $headers = "MIME-Version: 1.0\r\n";
+            $headers .= "Content-type: text/html; charset=utf-8\r\n";
+            $headers .= "From: Сайт " . $_SERVER['SERVER_NAME'] . " <" . $from . ">\r\n";
+
+            mail($from, 'Заявка на консультацию с сайта ' . $_SERVER['SERVER_NAME'], $message, $headers);
+
+            $response['mail'] = $message;
+        } else {
+            $response['success'] = 0;
+        }
+    }
+
+    echo json_encode($response);
+} else {
+
+    $template = reset($routeMap);
+
+
+    if (isset($routeMap[$uri])) {
+        $template = $routeMap[$uri];
+    }
+    
+    $template = 'competence';
+
+    include 'templates_1/' . $template . '.php';
+}
+
---
+++ templates_1/closed.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf-8" />
+        <title>TheQA - Российская компания по тестированию ПО</title>
+        <!--<meta name="viewport" content="width=device-width, minimum-scale=1.0">-->
+        <meta name="keywords" content="" />
+        <meta name="description" content="" />
+        <link rel="stylesheet" href="css/bootstrap.min.css"  />
+        <link rel="stylesheet" href="css/swiper.min.css"  />
+        <link rel="stylesheet" href="css/style.css"  />
+    </head>
+
+    <body>
+
+
+
+        <div class="wrapper">
+
+            <div class="header <?php echo $template == 'main' ? '' : 'black'; ?> ">
+
+                <div class="container-fluid">
+
+                    <div class="col-md-2">
+                        <a class="navbar-brand" href="<?php echo $baseLink; ?>/">
+                            <!--<img alt="Brand" src="img/logo.png" />-->
+                        </a>
+                    </div>
+                    <div class="col-md-10">
+
+                    </div>
+                </div>
+
+            </div><!-- .header-->
+
+            <div class="main-content">
+                <div class="container-fluid">
+
+                    <div class="row">
+
+                        <h1 class="text-center">
+                            The QA - Российская компания<br/>по тестированию ПО
+                        </h1>
+
+                        <!-- Swiper -->
+                        <div id="company-sliders" class="swiper-container1 info-sliders">
+                            <div class="swiper-wrapper1">
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <h2 class="text-center">
+                                    Сайт находится в разработке
+                                </h2>
+
+                            </div>
+                            <!-- Add Pagination -->
+                            <!--<div class="swiper-pagination wheel-enabled"></div>-->
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
+        </div><!-- .wrapper -->
+
+        <div class="footer">
+
+            
+        </div>
+
+        <script type="text/javascript" src="js/jquery.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/swiper.min.js"></script>
+        <script type="text/javascript" src="js/jquery.maskedinput.min.js"></script>
+        <script type="text/javascript" src="js/scripts.js"></script>
+    </body>
+</html>
