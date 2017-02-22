1d8577de9bb3622492248421840a2cc572e63fe6
Vysotskiy Alexander <vysotskij.av@gmail.com>
2017-02-22 13:38:55 +0200

    fixes

--- templates/header.php
+++ templates/header.php
@@ -9,7 +9,7 @@
         <meta name="description" content="" />
         <link rel="stylesheet" href="css/bootstrap.min.css?v=1"  />
         <link rel="stylesheet" href="css/swiper.min.css?v=1"  />
-        <link rel="stylesheet" href="css/style.css?v=10.7"  />
+        <link rel="stylesheet" href="css/style.css?v=10.8"  />
     </head>
 
     <body>
